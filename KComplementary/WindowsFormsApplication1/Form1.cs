f09455f76a36add3c4dd767252febb9a8bf19b35
Bill Teng <dbateng@hotmail.com>
2014-02-11 22:33:37 -0500

    K Complementary
    K Complementary Algorithm

---
+++ KComplementary/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int K = 6;
+            int[] A = {1, 8, -3, 0, 1, 3, -2, 4, 5};
+
+            int p = solution(6, A);
+
+            int c = p;
+        }
+
+        public int solution(int K, int[] A)
+        {
+            int len = A.Length;
+            int cnt = 0;
+
+            if (A.Length == 0)
+            {
+                return 0;
+            }
+
+            for (var i = 0; i < len; i++)
+            {
+                if (A[i]*2 == K) {
+                    cnt++;
+                }
+            }
+
+            for (var i = 0; i < len; i++) {
+                for (var j = i+1; j < len; j++) {
+                    if (A[i] + A[j] == K) {
+                        cnt += 2;
+                    }
+                }
+            }
+
+            return cnt;
+        }
+
+    }
+}
